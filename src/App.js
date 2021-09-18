df4e76a03188a374e26c705e3721ef5946a1ecf9
Bohdan <bodiasharko@gmail.com>
2021-09-18 22:39:26 +0300

    edit main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Header } from './Components/Header/Header';
-import { RootProvider } from './Provider/RootProvider';
-import { BrowserRouter } from 'react-router-dom';
-import { AppRouts } from './Router/AppRouts';
+import {Header} from './Components/Header/Header';
+import {RootProvider} from './Provider/RootProvider';
+import {BrowserRouter} from 'react-router-dom';
+import {AppRouts} from './Router/AppRouts';
 import {routs} from './Router/Routs';
 
 export const App = () => {
